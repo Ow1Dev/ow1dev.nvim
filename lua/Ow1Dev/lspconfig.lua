a623e5feb7f4f78dc1171955206e6edc9d0fc650
Ow1Dev <31339803+Ow1Dev@users.noreply.github.com>
2024-10-03 16:54:06 +0000

    feat: add come extra keybinds

--- lua/Ow1Dev/lspconfig.lua
+++ lua/Ow1Dev/lspconfig.lua
@@ -131,6 +131,11 @@ local function init()
 
 		lspconfig[server].setup(opts)
 	end
+
+  vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+  vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+  vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 end
 
 return {
